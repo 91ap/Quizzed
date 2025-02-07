a58a6ef7f30379898ce1c23b18e1794953657f00
Andy Patterson <andyp.dev@proton.me>
2025-02-07 04:23:53 -0600

    [Feature] Second struct created to allow for new format of multiple options for answers for questions and answer.

--- Model/Question.swift
+++ Model/Question.swift
@@ -14,6 +14,25 @@ struct Question {
     init(q: String, a: String) {
         text = q
         answer = a
+    
+        
     }
     
+   
+   
+    
 }
+
+struct MultipleChoiceQuestion {
+    let question: String
+    let choices: [String]
+    let correctAnswer: String
+    
+    init(q: String, c: [String], correctAnswer: String){
+        question = q
+        choices = c
+        self.correctAnswer = correctAnswer
+    }
+
+    }
+
