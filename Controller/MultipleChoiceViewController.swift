a793c58143db3ac5d94673bf5275564e2b13de37
Andy Patterson <andyp.dev@proton.me>
2025-02-07 04:19:44 -0600

    [Feature] MVC pattern updated to incorporate new feature that allows 2 quiz options now instead of 1. Second View controller added

--- Controller/MultipleChoiceViewController.swift
+++ Controller/MultipleChoiceViewController.swift
@@ -9,21 +9,71 @@ import UIKit
 
 class MultipleChoiceViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
+    @IBOutlet weak var firstChoiceButton: UIButton!
+    @IBOutlet weak var secondChoiceButton: UIButton!
+    @IBOutlet weak var thirdChoiceButton: UIButton!
+    
+    
+    
+    
+    
+    var multipleChoice = MultipleChoice()
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     override func viewDidLoad() {
+         super.viewDidLoad()
+
+         updateUI()
+     }
+
+     @IBAction func answerButtonPressed(_ sender: UIButton) {
+
+         let userAnswer = sender.currentTitle!
+         
+         let userGotItRight = multipleChoice.checkAnswer(userAnswer)
+
+         if userGotItRight {
+             sender.backgroundColor = UIColor.green
+             print("Correct!")
+         } else {
+             sender.backgroundColor = UIColor.red
+             print("o_O......Incorrect")
+         }
+         
+         multipleChoice.nextQuestion()
+
+         Timer
+             .scheduledTimer(
+                 timeInterval: 0.2,
+                 target: self,
+                 selector: #selector(updateUI),
+                 userInfo: nil,
+                 repeats: false
+             )
+
+     }
+
+     @objc func updateUI() {
+         questionLabel.text = multipleChoice.getQuestionText()
+         // Updated the progress view
+         progressBar.progress = multipleChoice.getProgress()
+         scoreLabel.text = "Score: \(multipleChoice.getScore())"
+         
+         firstChoiceButton.setTitle(multipleChoice.quiz[multipleChoice.questionNumber].choices[0], for: .normal)
+         secondChoiceButton.setTitle(multipleChoice.quiz[multipleChoice.questionNumber].choices[1], for: .normal)
+         thirdChoiceButton.setTitle(multipleChoice.quiz[multipleChoice.questionNumber].choices[2], for: .normal)
+
+         firstChoiceButton.backgroundColor = .clear
+         secondChoiceButton.backgroundColor = .clear
+         thirdChoiceButton.backgroundColor = .clear
+         
+         
+
+        
+     }
 
 }
